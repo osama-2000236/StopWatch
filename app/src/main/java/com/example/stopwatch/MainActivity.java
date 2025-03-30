8d7ca97cf3044ec8ee1d306e1664a2317a8655f6
osamaabujarad <1202883@student.birzeit.edu>
2025-03-30 07:37:53 +0300

    my ass id:1202883 Name:Osama Abujarad

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.SystemClock;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvTime;
-    private Button btnStart, btnStop, btnReset;
+    private Button btnStart, btnStop, btnReset, btnrotate;
     private Handler handler;
     private long startTime = 0L, timeInMilliseconds = 0L, timeSwapBuff = 0L, updateTime = 0L;
+    private boolean isRunning = false;
     private Runnable updateTimerThread;
 
     @Override
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStart);
         btnStop = findViewById(R.id.btnStop);
         btnReset = findViewById(R.id.btnReset);
+        btnrotate = findViewById(R.id.btnrotate);
 
         handler = new Handler();
 
@@ -33,22 +39,48 @@ public class MainActivity extends AppCompatActivity {
                 timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
                 updateTime = timeSwapBuff + timeInMilliseconds;
                 updateDisplay();
-                handler.postDelayed(this, 1000); // Update every 1 second
+                handler.postDelayed(this, 1000); // update every 1 second
             }
         };
 
+
+        if (savedInstanceState != null) {
+            startTime = savedInstanceState.getLong("startTime");
+            timeInMilliseconds = savedInstanceState.getLong("timeInMilliseconds");
+            timeSwapBuff = savedInstanceState.getLong("timeSwapBuff");
+            updateTime = savedInstanceState.getLong("updateTime");
+            isRunning = savedInstanceState.getBoolean("isRunning");
+            updateDisplay();
+            if (isRunning) {
+                // Adjust startTime so that the elapsed time remains accurate
+                startTime = SystemClock.uptimeMillis() - timeInMilliseconds;
+                handler.post(updateTimerThread);
+            }
+            // Set buttons based on the running state
+            updateButtonStates(!isRunning, true, isRunning, !isRunning);
+        } else {
+            // Initial button states (start enabled, others disabled)
+            updateButtonStates(true, true, false, false);
+        }
+
         // Start Button Click Event
         btnStart.setOnClickListener(v -> {
-            startTime = SystemClock.uptimeMillis();
-            handler.post(updateTimerThread);
-            updateButtonStates(false, true, true);
+            if (!isRunning) {
+                startTime = SystemClock.uptimeMillis();
+                handler.post(updateTimerThread);
+                isRunning = true;
+                updateButtonStates(false, true, true, true);
+            }
         });
 
         // Stop Button Click Event
         btnStop.setOnClickListener(v -> {
-            timeSwapBuff += timeInMilliseconds;
-            handler.removeCallbacks(updateTimerThread);
-            updateButtonStates(true, false, true);
+            if (isRunning) {
+                timeSwapBuff += timeInMilliseconds;
+                handler.removeCallbacks(updateTimerThread);
+                isRunning = false;
+                updateButtonStates(true, true, false, true);
+            }
         });
 
         // Reset Button Click Event
@@ -56,11 +88,15 @@ public class MainActivity extends AppCompatActivity {
             startTime = timeSwapBuff = timeInMilliseconds = updateTime = 0L;
             updateDisplay();
             handler.removeCallbacks(updateTimerThread);
-            updateButtonStates(true, false, false);
+            isRunning = false;
+            updateButtonStates(true, true, false, false);
         });
+
+        // Rotate Button Click Event
+        btnrotate.setOnClickListener(v -> rotateDevice(v));
     }
 
-    // Helper method to update the displayed time
+    // Update the displayed time in mm:ss format
     private void updateDisplay() {
         int secs = (int) (updateTime / 1000);
         int mins = secs / 60;
@@ -68,10 +104,33 @@ public class MainActivity extends AppCompatActivity {
         tvTime.setText(String.format("%02d:%02d", mins, secs));
     }
 
-    // Helper method to enable/disable buttons
-    private void updateButtonStates(boolean start, boolean stop, boolean reset) {
+    // Enable/disable buttons: parameters for start, rotate, stop, reset respectively
+    private void updateButtonStates(boolean start, boolean rotate, boolean stop, boolean reset) {
         btnStart.setEnabled(start);
+        btnrotate.setEnabled(rotate);
         btnStop.setEnabled(stop);
         btnReset.setEnabled(reset);
+    }
+
+    // Save the timer state so that it can be restored on rotation
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("startTime", startTime);
+        outState.putLong("timeInMilliseconds", timeInMilliseconds);
+        outState.putLong("timeSwapBuff", timeSwapBuff);
+        outState.putLong("updateTime", updateTime);
+        outState.putBoolean("isRunning", isRunning);
+    }
+
+    // Toggle device orientation when the rotate button is clicked
+    public void rotateDevice(View view) {
+        Log.d("MainActivity", "rotateDevice called");
+        int currentOrientation = getResources().getConfiguration().orientation;
+        if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
     }
 }
